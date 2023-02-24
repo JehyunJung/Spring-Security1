9dca44dc3aa2af288116aa5676be443b578758a9
jehyunJung <toojey7@gmail.com>
2023-02-24 19:18:00 +0900

    study: spring security exception handling
    session control
    exception handling
    csrf

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -6,30 +6,71 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import java.io.IOException;
+import java.net.ResponseCache;
 
-@Configuration
-@EnableWebSecurity
+/*@Configuration
+@EnableWebSecurity*/
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, sys, admin);
+
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         //인증 여부 처리
         httpSecurity
                 .authorizeHttpRequests()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/user").hasRole("USER")
+                .requestMatchers("/admin/pay").hasRole("ADMIN")
+                .requestMatchers("/admin/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') or hasRole('SYS')"))
                 .anyRequest().authenticated();
+
+
         //로그인 설정
         httpSecurity
                 .formLogin()
@@ -43,7 +84,12 @@ public class SecurityConfig {
                     @Override
                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
                         System.out.println("authentication: " + authentication.getName());
-                        response.sendRedirect("/");
+                        //인증이 완료된 이후에 기존 RequestCache 안에 저장되어 있는 redirect url 정보를 받아서 해당 url로 redirect 시킨다.
+                        RequestCache requestCache = new HttpSessionRequestCache();
+                        SavedRequest savedRequest = requestCache.getRequest(request, response);
+                        String redirectUrl = savedRequest.getRedirectUrl();
+                        response.sendRedirect(redirectUrl);
+
                     }
                 })
                 .failureHandler(new AuthenticationFailureHandler() {
@@ -57,7 +103,7 @@ public class SecurityConfig {
         //logout 설정
         httpSecurity.logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
+                .logoutSuccessUrl("/")
                 .deleteCookies("JSESSIONID", "remember-me")
                 .addLogoutHandler(new LogoutHandler() {
                     @Override
@@ -69,7 +115,7 @@ public class SecurityConfig {
                 .logoutSuccessHandler(new LogoutSuccessHandler() {
                     @Override
                     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
+                        response.sendRedirect("/");
                     }
                 });
         //리멤버-미 처리(사용자의 로그인 기록 유지)
@@ -77,6 +123,29 @@ public class SecurityConfig {
                 .rememberMeParameter("remember")
                 .tokenValiditySeconds(3600);
 
+        //세션 제어
+        httpSecurity.sessionManagement()
+                .maximumSessions(-1)
+                .maxSessionsPreventsLogin(false);
+        //세션 고정 보호
+        httpSecurity.sessionManagement()
+                .sessionFixation().changeSessionId()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);    //세션 정책
+
+        //예외 처리 기능
+        httpSecurity.exceptionHandling()
+                /*.authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        response.sendRedirect("/login"); //spring security가 제공하는 login이 아닌 사용자가 정의한 login page로 이동하기 때문에, login 요청에 대한 url mapping이 필요하며, 인증 절차에서 permitAll 처리가 요구된다.
+                    }
+                })*/
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        response.sendRedirect("/denied");
+                    }
+                });
         return httpSecurity.build();
     }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,6 +1,7 @@
 package io.security.basicsecurity;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,9 +10,40 @@ public class SecurityController {
     public String index() {
         return "home";
     }
+    @PostMapping("/")
+    public String postIndeX() {
+        return "home";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
 
     @GetMapping("/loginPage")
     public String loginPage() {
         return "loginPage";
     }
+
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+    }
+
+    @GetMapping("/admin/**")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    /*@GetMapping("/denied")
+    public String denied() {
+        return "Access is denied";
+    }*/
+
+
 }
