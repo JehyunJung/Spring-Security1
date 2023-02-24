af54277edf291fe6b5d1e409225849ea85143575
jehyunJung <toojey7@gmail.com>
2023-02-24 21:37:51 +0900

    study: spring security filter flows
    delegatingfilterproxy, filterchainproxy
    multiple security configs
    authentication

---
+++ src/main/java/io/security/basicsecurity/MultipleSecurityConfigs.java
@@ -0,0 +1,59 @@
+package io.security.basicsecurity;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class MultipleSecurityConfigs {
+
+    @Bean
+    @Order(0)
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        //인증 여부 처리
+        httpSecurity
+                .securityMatcher("/admin/**")
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain filterChain2(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin();
+        return httpSecurity.build();
+    }
+}
